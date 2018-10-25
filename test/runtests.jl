9a47bb869ef5a3ee6da68bd690c85f07e415fcc8
Martin Biel <martinbiel93@gmail.com>
2018-10-25 10:03:21 +0200

    CI testing on test branch

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,13 @@ using Test
 using Distributed
 using JuMP
 using StochasticPrograms
-using Gurobi
+using Ipopt
+using GLPKMathProgInterface
 
 τ = 1e-5
-reference_solver = GurobiSolver(OutputFlag=0)
-phsolvers = [(ProgressiveHedgingSolver(:ph,GurobiSolver(OutputFlag=0),log=false),"Progressive Hedging")]
+reference_solver = GLPKSolverLP()
+qp_solver = IpoptSolver(print_level=0)
+phsolvers = [(ProgressiveHedgingSolver(:ph,qp_solver,log=false),"Progressive Hedging")]
 
 problems = Vector{Tuple{JuMP.Model,String}}()
 @info "Loading test problems..."
@@ -17,23 +19,24 @@ include("simple.jl")
 include("farmer.jl")
 @info "Loading infeasible..."
 include("infeasible.jl")
-@info "Loading integer..."
-include("integer.jl")
-
 @info "Test problems loaded. Starting test sequence."
-@testset "$phname Solver: $name" for (phsolver,phname) in phsolvers, (sp,name) in problems
-    solve(sp,solver=reference_solver)
-    x̄ = optimal_decision(sp)
-    Q̄ = optimal_value(sp)
-    solve(sp,solver=phsolver)
-    @test abs(optimal_value(sp) - Q̄) <= τ*(1e-10+abs(Q̄))
+
+@testset "Sequential solver" begin
+    @testset "$phname Solver: $name" for (phsolver,phname) in phsolvers, (sp,name) in problems
+        solve(sp,solver=reference_solver)
+        x̄ = optimal_decision(sp)
+        Q̄ = optimal_value(sp)
+        solve(sp,solver=phsolver)
+        @test abs(optimal_value(sp) - Q̄) <= τ*(1e-10+abs(Q̄))
+    end
 end
 
 @info "Starting distributed tests..."
 
-include("/usr/share/julia/test/testenv.jl")
+include(joinpath(Sys.BINDIR, "..", "share", "julia", "test", "testenv.jl"))
+disttestfile = joinpath(@__DIR__, "run_dtests.jl")
 push!(test_exeflags.exec,"--color=yes")
-cmd = `$test_exename $test_exeflags run_dtests.jl`
+cmd = `$test_exename $test_exeflags $disttestfile`
 
 if !success(pipeline(cmd; stdout=stdout, stderr=stderr)) && ccall(:jl_running_on_valgrind,Cint,()) == 0
     error("Distributed test failed, cmd : $cmd")
